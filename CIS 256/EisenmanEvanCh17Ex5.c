8215cb819e7d0cdc6102975d368fed3b5ab39673
Evan <monkeyofthespud@gmail.com>
2025-10-25 16:46:13 -0700

    added comments to homework

--- CIS 256/EisenmanEvanCh17Ex5.c
+++ CIS 256/EisenmanEvanCh17Ex5.c
@@ -3,10 +3,12 @@
 //Chapter 17 exercise 5
 //Ask a user to enter a series of words and stops when the user enters an empty string then sorts the enterend words alphabetically
 
+//directives to include the standard input output, standard libary,and string libary so we can use functions from them
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
+//two macros that define both the max length of a string and the max strings we can store
 #define MAX_STR_LEN 20
 #define MAX_WORDS 50
 
@@ -33,41 +35,43 @@ int read_string(char *inputArray,int arrayLength)
     //returns the length of the array
     return charCount;
 }
+//comparison function to pass to q sort
 int compare_strings(const void *element1, const void *element2)
 {
-    return strcmp(*(char **)element1,*(char **)element2);
+    return strcmp(*(char **)element1,*(char **)element2);//derefrences the pointer to a pointer and calls and string comparison function and passes that value back to qsort
 }
 
 int main(void)
 {
-    char inputString[MAX_STR_LEN+1];
-    char * wordList[MAX_WORDS]={NULL};
-    char * dynaString;
-    int wordCount = 0,inputCharCount=0;
+    char inputString[MAX_STR_LEN+1];//string that will hold the users input
+    char * wordList[MAX_WORDS]={NULL};//array that will store pointers to strings
+    char * dynaString;//character pointer that will hold the dynamic string
+    int wordCount = 0,inputCharCount=0;//int var that will hold the ammount of characters entered by the user and the ammount of total words
     
+    //prompts user to enter a word and loops until they hit the max # of words or enter a empty string
     printf("Enter a word: ");
     while (wordCount < MAX_WORDS && (inputCharCount = read_string(&inputString[0],MAX_STR_LEN+1)))
     {
-        if((dynaString = malloc(inputCharCount+1))==NULL)
+        if((dynaString = malloc(inputCharCount+1))==NULL)//checks if malloc returned null and exist if it did
         {
             printf("Error:malloc returned a null pointer");
             exit(EXIT_FAILURE);
         }
-        strcpy(dynaString,inputString);
+        strcpy(dynaString,inputString);//copys the string from the user into the dynamic string
         
-        wordList[wordCount]=dynaString;
+        wordList[wordCount]=dynaString;//stores the pointer of the dynamic stromg into our word list array
         
-        wordCount ++;
+        wordCount ++;//itterates the word count one more
         
-        if (wordCount<MAX_WORDS)
+        if (wordCount<MAX_WORDS)//checks if we are on the last item of the array if we arent we ask the user to enter another word
         {
             printf("Enter a word: ");
         }
     }
-    qsort(wordList,wordCount,sizeof(char*),compare_strings);
-    wordCount = 0;
+    qsort(wordList,wordCount,sizeof(char*),compare_strings);//calls qsort passing our array and the ammount of words we entred into it with the size of the char pointer and pass the pointer to our compare_strings func
+    wordCount = 0;//sets word count back to zero so we can loop throug it
     printf("Sorted Order\n");
-    while (wordCount < MAX_WORDS && wordList[wordCount] != NULL)
+    while (wordCount < MAX_WORDS && wordList[wordCount] != NULL)//loops through the range of the array until the first null pointer prints the value at each index which are now sorted
     {
         printf("%d: %s\n",wordCount+1,wordList[wordCount]);
         wordCount ++;
